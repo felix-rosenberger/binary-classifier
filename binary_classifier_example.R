e36d6b199d85b5b8c5ec0d9ab1f61e132396186f
felix-rosenberger <74730991+felix-rosenberger@users.noreply.github.com>
2023-03-22 13:05:23 +1100

    loss function plot for different learning rates

--- binary_classifier_example.R
+++ binary_classifier_example.R
@@ -1,6 +1,9 @@
 # first load required packages
 library(tidyverse)
 library(caret)
+library(ggplot2)
+library(reshape2)
+source("binary_classifier.R") # load file containing the classifier functions
 
 # read data
 data = read.csv("BinaryClassifier.csv", header = FALSE)
@@ -42,109 +45,7 @@ head(predicted.classes)
 # confusion matrix
 confusionMatrix(factor(predicted.classes),factor(test$y), mode = "everything")
 
-binary.classifier.train = function(
-    train.df, # training dataframe, labels have to be named "y"
-    theta = rep(1, ncol(train)), # vector of weights dependent on training data size
-    alpha, # numerical value, learning rate for optimisation
-    epsilon = 0.000001, # convergence tolerance for optimisation
-    iter.max = 1000) { # maximum number of iterations for optimisation
-  
-  # preprocess input data
-  X = train[, colnames(train) != "y"] # select features only
-  X = scale(X) # standardise features
-  col.means = attr(X,"scaled:center") # save feature means for test data standardisation
-  col.std = attr(X,"scaled:scale") # save feature standard deviation for test data
-  X = as.matrix( # convert final output of training data to matrix
-    cbind( # combine bias term and training data
-      rep(1, nrow(train)), X), # create sequence of bias terms and combine with scaled data
-    ncol = ncol(train))
-  y = matrix(train[, colnames(train) == "y"], ncol = 1) # create label vector
-  
-  # initialise parameters
-  res.epsilon = 1 # tolerance
-  res.theta = theta # parameter weights
-  iter = 1 # iteration
-  theta = matrix(theta, ncol = 1) # transform vector into matrix
-  
-  # cost function definition
-  y_hat = 1 / (1 + exp(-(X %*% theta))) # define sigmoid function for prediction 
-  res.cost = -sum(y * log(y_hat) + (1 - y) * log(1 - y_hat)) # define binary CE loss function and initial loss
-  
-  # model training
-  while (res.epsilon > epsilon & iter < iter.max) { 
-    y_hat = 1 / (1 + exp(-(X%*%theta))) # define again for each prediction
-    theta.new = theta - alpha * (t(X) %*% (y_hat - y)) # determine new theta using CE gradient and learning rate
-    res.theta = cbind(res.theta, theta.new) # for each iteration, add new weights
-    cost = -sum(y * log(y_hat) + (1 - y) * log(1 - y_hat)) # determine cost for current weights
-    res.cost = cbind(res.cost, cost) # for each iteration, add cost for respective weights
-    res.epsilon = sum((theta-theta.new)**2)^0.5 # determine tolerance at each iteration and update
-    theta = theta.new # update weights for next iteration
-    iter = iter + 1 # update counter for iterations
-  }
-  #create result list for later extraction
-  result = list(final.weight = theta, 
-                weights = res.theta, 
-                final.cost = cost,
-                cost = res.cost,
-                iterations = iter,
-                tolerance = res.epsilon,
-                train.means = col.means,
-                train.std = col.std,
-                learning.rate = alpha)
-  return(result)
-  
-}
-
-binary.classifier.predict = function(test.df, # test dataframe, labels have to be named "y"
-                                     trained.classifier, # trained classifier object
-                                     threshold = 0.5) { # set prediction threshold
-  # preprocess input data
-  X = test[, colnames(test) != "y"]
-  # standardise each feature column in test data (mean and std estimates from training data)
-  for (i in 1:ncol(X)) {
-    X[,i] = scale(X[,i], 
-                  center = trained.classifier$train.means[i], 
-                  scale = trained.classifier$train.std[i])
-  }
-  X = as.matrix( # convert final output of test data to matrix
-    cbind( # combine bias term and test data
-      rep(1, nrow(test)), X), # create sequence of bias terms and combine with scaled data
-    ncol = ncol(test))
-  y = matrix(test[, colnames(train) == "y"], ncol = 1)
-  
-  # predict output based on optimal weights
-  theta = trained.classifier$final.weight
-  y.pred = 1 / (1 + exp(-(X%*%theta)))
-  
-  # convert probabilities into classes
-  y.class = matrix(NA, nrow = nrow(y.pred), ncol = 1)
-  for (i in 1:nrow(y.pred)) {
-    if (y.pred[i] > threshold) {
-      y.class[i] = 1
-    }
-    else {
-      y.class[i] = 0
-    }
-  }
-  
-  # model evaluation
-  if (!require("pacman")) install.packages("pacman") # wrapper, checks if packages are installed; 
-  #installs if required
-  pacman::p_load(caret)
-  
-  # confusion matrix for later retrieval
-  y.true = test.df[, colnames(test) == "y"] # save true labels
-  conf.matrix = confusionMatrix(factor(y.class),factor(y.true), mode = "everything")
-  
-  result = list(pred.probabilities = y.pred, # save probabilities for each test observation
-                pred.classes = y.class, # save predicted class for each test observation
-                true.classes = y.true, # save true classes for retrieval and comparison
-                conf.matrix = conf.matrix, # save confusion matrix
-                weights = theta) # save weights used
-  return(result)
-}
-
-# train for different alphas
+# train for different learning rates
 alphas = seq(0.01, 0.1, by = 0.02) # define sequence of alphas
 costs = data.frame(matrix(NA, nrow = 1000, ncol = length(alphas))) # create empty df with nrow = max.iter
 final.costs = c()
@@ -193,6 +94,49 @@ abline(h=final.costs[5],col="red")
 legend(700, 600, legend=c(paste("Final Cost: ", round(final.costs[5], 0)), "Loss Function"),
        col=c("red", "black"), lty=1:2, cex=0.8,
        box.lty=0)
+
+# alternatively make nice plot with ggplot
+# change to long format
+costs.long = melt(costs, variable.name = "Alpha", value.name = "Cost")
+costs.long$Iteration = sequence(rep(nrow(costs), ncol(costs))) # add iterations for each alpha
+
+# remove rows with missing values
+costs.long = costs.long[!is.na(costs.long$Cost),]
+
+# Create a data frame with the final iteration count for each alpha
+final.iterations = data.frame(Alpha = unique(costs.long$Alpha),
+                               FinalIteration = tapply(costs.long$Iteration, costs.long$Alpha, 
+                                                       max)) # selecting max iteration value per alpha
+
+# find three best learning rates based on final costs
+best.alpha = final.iterations[order(final.iterations$FinalIteration), "Alpha"][1:3]
+
+# max number of iterations among three best rates
+max.iter = max(final.iterations$FinalIteration[final.iterations$Alpha %in% best.alpha])
+
+# set x range
+x.range = max.iter + 10
+
+# find maximum cost at last iteration
+cost.final = merge(costs.long, final.iterations, by = "Alpha")
+costs.finaliter = subset(cost.final, Iteration == FinalIteration)
+max.cost = max(costs.finaliter$Cost)
+min.cost = min(costs.finaliter$Cost)
+y.max = max.cost + 10
+y.min = min.cost - 10
+
+# create plot with loss functions over learning rate range
+ggplot(costs.long, aes(x = Iteration, y = Cost, color = Alpha)) +
+  #geom_point() +
+  geom_line() +
+  scale_color_discrete(name = "Learning Rate") +
+  labs(title = "Loss Functions for Different Learning Rates",
+       subtitle = "Binary Cross-Entropy Loss",
+       x = "Iteration",
+       y = "Cost") +
+  coord_cartesian(xlim = c(0, x.range),
+                  ylim = c(y.min, y.max)) + # set axis limit
+  theme_minimal()
 
 # training with optimal alpha value
 binary.train = binary.classifier.train(train, alpha = 0.03)
