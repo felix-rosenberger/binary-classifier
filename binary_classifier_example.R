0f53d61cefb10b5efee77b4e6ef7d787e568cdde
felix-rosenberger <74730991+felix-rosenberger@users.noreply.github.com>
2023-03-22 16:13:15 +1100

    minor adjustments after removing visualisation function

--- binary_classifier_example.R
+++ binary_classifier_example.R
@@ -45,79 +45,14 @@ head(predicted.classes)
 # confusion matrix
 confusionMatrix(factor(predicted.classes),factor(test$y), mode = "everything")
 
-loss.function.visualisation = function(min.alpha, # smallest learning rate
-                                       max.alpha, # highest learning rate
-                                       increments, # increments for learning rate to train model
-                                       max.iter = 1000){
-  # train for different learning rates
-  alphas = seq(min.alpha, max.alpha, by = increments) # define sequence of alphas
-  costs = data.frame(matrix(NA, nrow = max.iter, 
-                            ncol = length(alphas))) # create empty df with nrow = max.iter
-  final.costs = c()
-  init.iter = max.iter
-  optimal.alpha = 0
-  # for each alpha, train classifier and add costs as column to results df
-  for (i in 1:length(alphas)) {
-    classifier = binary.classifier.train(train, alpha = alphas[i], iter.max = max.iter)
-    cost = c(classifier$cost) # costs history of classifier for each alpha
-    final.costs = cbind(final.costs, classifier$final.cost)
-    costs[,i] = c(cost, rep(NA, nrow(costs) - length(cost))) # append costs to results df
-    names(costs)[i] = paste("alpha =", alphas[i]) # rename column to alpha level
-    iter = classifier$iterations # extract iterations for alpha
-    # update optimal learning rate based on convergence speed
-    if (iter <= init.iter){
-      optimal.alpha = alphas[i]
-      init.iter = iter
-    }
-  }
-  
-  # plot loss functions with ggplot
-  # change to long format
-  suppressWarnings({
-    costs.long = melt(costs, variable.name = "Alpha", value.name = "Cost")
-    })
-  costs.long$Iteration = sequence(rep(nrow(costs), ncol(costs))) # add iterations for each alpha
-  costs.long = costs.long[!is.na(costs.long$Cost),] # remove rows with missing values
-  
-  # Create a data frame with the final iteration count for each alpha
-  final.iterations = data.frame(Alpha = unique(costs.long$Alpha),
-                                FinalIteration = tapply(costs.long$Iteration, costs.long$Alpha, 
-                                                        max)) # selecting max iteration value per alpha
-  best.alpha = final.iterations[order(final.iterations$FinalIteration), 
-                                "Alpha"][1:3] # 3 best learning rates based on convergence speed
-  max.iter = max(final.iterations$FinalIteration[final.iterations$Alpha %in% best.alpha]) # get maximum iterations from best alphas
-  x.range = max.iter + 10 # set x range
-  
-  # find maximum cost at last iteration
-  cost.final = merge(costs.long, final.iterations, by = "Alpha") # show max number of iterations on every row for each alpha
-  costs.finaliter = subset(cost.final, Iteration == FinalIteration) # keep only those with max iteration to keep costs
-  max.cost = max(costs.finaliter$Cost) 
-  min.cost = min(costs.finaliter$Cost)
-  # set y range
-  y.max = max.cost + 10
-  y.min = min.cost - 1
-  
-  # create plot with loss functions over learning rate range
-  plot = ggplot(costs.long, aes(x = Iteration, y = Cost, color = Alpha)) +
-    geom_line() +
-    scale_color_discrete(name = "Learning Rate") +
-    labs(title = "Loss Visualisation for Different Learning Rates",
-         subtitle = "Binary Cross-Entropy Loss Function",
-         x = "Iteration",
-         y = "Cost") +
-    coord_cartesian(xlim = c(0, x.range),
-                    ylim = c(y.min, y.max)) + # set axis limit
-    theme_minimal()
-  
-  # create result output
-  result = list(loss.plot = plot,
-                optimal.alpha = optimal.alpha)
-  
-  return(result)
-}
+# check classifier learning rates
+loss.visualisation = loss.function.visualisation(min.alpha = 0.01,
+                                                 max.alpha = 0.1,
+                                                 increments = 0.01,
+                                                 max.iter = 1000)
 
 # training with optimal alpha value
-binary.train = binary.classifier.train(train, alpha = 0.03)
+binary.train = binary.classifier.train(train, alpha = loss.visualisation$optimal.alpha)
 
 # make predictions on test set
 binary.predict = binary.classifier.predict(test, binary.train)
